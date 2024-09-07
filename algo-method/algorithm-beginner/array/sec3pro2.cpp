ea343484b8718218013aadf7d3bc78e8f4dcefd1
nao317 <country.gentleman.0317@gmail.com>
2024-09-07 11:55:20 +0900

    「間違い探し」コメント追加とインデント整形

--- algo-method/algorithm-beginner/array/sec3pro2.cpp
+++ algo-method/algorithm-beginner/array/sec3pro2.cpp
@@ -6,37 +6,37 @@ using namespace std;
 int main()
 {
 
-    int h, w;
-    cin >> h >> w;
+    int h, w;      // 行数hと列数w
+    cin >> h >> w; // 入力
 
-    vector<string> s(h);
-    vector<string> t(h);
+    vector<string> s(h); // 一つ目の表
+    vector<string> t(h); // 二つ目の表
 
     for (int i = 0; i < h; i++)
     {
-        cin >> s[i];
+        cin >> s[i]; // 一つ目の表に入力（行ごとに文字列として）
     }
     for (int j = 0; j < h; j++)
     {
-        cin >> t[j];
+        cin >> t[j]; // 二つ目の表に入力（行ごとに文字列として）
     }
 
-    int count = 0;
+    int count = 0; // 間違い探し用の整数型変数countを宣言
 
     for (int a = 0; a < h; a++)
     {
-        string str1 = s[a];
-        string str2 = t[a];
+        string str1 = s[a]; // 一つ目の表のa行目の文字列を宣言した文字列型変数str1に代入
+        string str2 = t[a]; // 二つ目の表のa行目の文字列を宣言した文字列型変数str2に代入
         for (int b = 0; b < w; b++)
         {
-            if (str1[b] != str2[b])
+            if (str1[b] != str2[b]) // 一文字ずつ比較する
             {
-                count++;
+                count++; // 同じ文字でないときインクリメント
             }
         }
     }
 
-    cout << count << endl;
+    cout << count << endl; // 出力
 
-    return 0;
+    return 0; // 終了
 }
