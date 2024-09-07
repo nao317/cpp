070f97f13bdf952a18fd2a86006731743b142fbc
nao317 <country.gentleman.0317@gmail.com>
2024-09-07 17:03:21 +0900

    「地図の切り取り」コメント追加

--- algo-method/algorithm-beginner/array/sec3pro3.cpp
+++ algo-method/algorithm-beginner/array/sec3pro3.cpp
@@ -13,30 +13,30 @@ void input(vector<string> &vec, int h) // input関数の宣言（vecに文字列
 
 void cutout(vector<string> &vec, vector<string> &result, int x, int y) // 該当箇所を切り出すためのcutout関数を作成
 {
-    int upper = x - 1;
-    int left = y - 1;
-    for (int a = 0; a < 3; a++)
+    int upper = x - 1; // 入力後の配列の一行目の一文字目に当たる文字のインデックス（y座標）
+    int left = y - 1;  // 入力後の配列の一行目の一文字目に当たる文字のインデックス（x座標）
+    for (int a = 0; a < 3; a++) // 該当箇所を切り出す
     {
-        string str = vec[upper + a];
-        string cut = str.substr(left, left + 2);
-        result[a] = cut;
+        string str = vec[upper + a]; // 一旦文字列に置き換える
+        string cut = str.substr(left, left + 2); // ３文字分leftで求めたインデックスから切り取る
+        result[a] = cut; // 切り取った文字列をresult[a]に代入
     }
 }
 
 int main()
 {
 
-    int h, w, x, y;
-    cin >> h >> w >> x >> y;
+    int h, w, x, y; // 整数型変数を宣言
+    cin >> h >> w >> x >> y; // 整数型変数たちに入力
 
     vector<string> vec(h);    // parameter用配列
     vector<string> result(3); // 解答用配列
     input(vec, h);            // vecへ入力
-    cutout(vec, result, x, y);
+    cutout(vec, result, x, y); // 文字列を切り取る
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++) // 出力
     {
         cout << result[i] << endl;
     }
-    return 0;
+    return 0; // 終了
 }
