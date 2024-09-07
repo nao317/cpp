1186719f5a6c308dac384c137c89753006644902
nao317 <country.gentleman.0317@gmail.com>
2024-09-07 11:48:27 +0900

    「単発のクエリ」インデント整形

--- algo-method/algorithm-beginner/array/sec1pro2.cpp
+++ algo-method/algorithm-beginner/array/sec1pro2.cpp
@@ -3,8 +3,9 @@
 
 using namespace std;
 
-int main () {
-    vector<int> vec{3,1,4,1,5,9,2,6,5,3}; // vectorの宣言と要素の定義
+int main()
+{
+    vector<int> vec{3, 1, 4, 1, 5, 9, 2, 6, 5, 3}; // vectorの宣言と要素の定義
     int k;
 
     cin >> k; // 単発のクエリ
@@ -12,5 +13,4 @@ int main () {
     cout << vec[k] << endl; // クエリで指定された要素を出力
 
     return 0; // 終了
-
 }
