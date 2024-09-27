ee6e5be6afef032ff06bbc5f5cbf7822589f6e98
nao317 <country.gentleman.0317@gmail.com>
2024-09-27 12:31:33 +0900

    データ構造とアルゴリズムの予習+アルゴ式（全探索）

---
+++ algo-method/algorithm-beginner/allsearch/sec4pro1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<int> vec1(n);
+    vector<int> vec2(m);
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> vec1[i];
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+
+        cin >> vec2[i];
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < m; j++)
+        {
+
+            if (vec1[i] > vec2[j])
+            {
+
+                count++;
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
