258ad733676e4ebcf69a24e8a860902ef28d1319
nao317 <country.gentleman.0317@gmail.com>
2025-03-04 15:57:53 +0900

    renew subbranch abc002_c.cpp

--- atcoder/abc/abc002_c.cpp
+++ atcoder/abc/abc002_c.cpp
@@ -1,6 +1,7 @@
 /* C - 直訴 */
 
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
@@ -11,5 +12,8 @@ int main(void) {
     double AB = sqrt(pow(ax - bx, 2) + pow(ay - by, 2));
     double AC = sqrt(pow(ax - cx, 2) + pow(ay - cy, 2));
     double BC = sqrt(pow(bx - cx, 2) + pow(by - cy, 2));
-    double s = AB + AC + BC;
+    double s = (AB + AC + BC) / 2;
+    double S = sqrt(s * (s - AB) * (s - AC) * (s - BC));
+    cout << fixed << setprecision(1) << S << endl;
+    return 0;
 }
