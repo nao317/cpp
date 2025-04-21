ba09371d7123feabad7068af0b1e5064ccc69064
nao317 <country.gentleman.0317@gmail.com>
2025-04-21 11:00:44 +0900

    一日置いたらACできた。条件分岐を工夫する。

--- atcoder/abc/abc398_a.cpp
+++ atcoder/abc/abc398_a.cpp
@@ -1,18 +1,32 @@
-/* A -  Doors in the Center */
+/* A - Doors in the Center */
 
 #include <iostream>
 
 using namespace std;
 
 int main(void) {
+
     int n;
     cin >> n;
-    int status = n / 2;
+
+    // ここでは偶数判定をする
     int flag = n % 2;
-    for (int i= 1; i <= n; i++) {
-        if (i == status || i == status + flag) {
-            cout << "=";
+
+    // 中間地点
+    int center = n / 2;
+
+    for (int i = 1; i <= n; i++) {
+        if (flag == 0) {
+            if (center == i || center + 1 == i) {
+                cout << "=";
+                continue;
+            }
+            cout << "-";
         } else {
+            if (center + 1 == i) {
+                cout << "=";
+                continue;
+            }
             cout << "-";
         }
     }
